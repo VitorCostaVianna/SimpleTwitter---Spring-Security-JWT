1d4fabdd0e82cd5917cea0aa61b5358b30d6d0c3
VitorCostaVianna <vcvianna@sga.pucmminas.br>
2024-12-19 12:19:20 -0300

    Automating admin creation

---
+++ springsecurity/springsecurity/src/main/java/com/vitor/springsecurity/config/AdminUserConfig.java
@@ -0,0 +1,54 @@
+package com.vitor.springsecurity.config;
+
+import java.util.Set;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.vitor.springsecurity.entities.Role;
+import com.vitor.springsecurity.entities.User;
+import com.vitor.springsecurity.repository.RoleRepository;
+import com.vitor.springsecurity.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Configuration
+public class AdminUserConfig {
+    
+    private RoleRepository roleRepository;
+
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public AdminUserConfig(RoleRepository roleRepository,
+                           UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+       var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
+
+       var userAdmin = userRepository.findByUsername("admin");
+
+       userAdmin.ifPresentOrElse(
+            (user) -> {
+                System.out.println("admin already exists");
+            },
+            () -> {
+                    var user = new User();
+                    user.setUsername("admin");
+                    user.setPassword(passwordEncoder.encode("123"));
+                    user.setRoles(Set.of(roleAdmin));
+                    userRepository.save(user);
+            }
+       
+       );
+    }
+}
